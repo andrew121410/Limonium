c3472a1569530254ebe70cf211e160001e03be39
Andrew121410 <andrewcherring@gmail.com>
2023-06-20 01:09:05 -0400

    More work

--- src/api/viaversion/mod.rs
+++ src/api/viaversion/mod.rs
@@ -63,71 +63,9 @@ impl platform::IPlatform for ViaVersionAPI {
     }
 
     async fn custom_download_functionality(&self, project: &String, version: &String, build: &String, link: &String) -> Option<String> {
-        // Download the .zip file to the temp directory
-        let zip_file_path = env::temp_dir().join("limonium-via.zip");
-        let response = reqwest::get(link).await.expect("Failed to send request.");
-        let bytes = response.bytes().await.expect("Failed to get bytes.");
-        fs::write(&zip_file_path, &bytes).expect("Failed to write file.");
+        let file_name = api::jenkins_artifacts_bundle_zip_download_and_find_jar_and_place_jar_in_the_tmp_directory(&project, &version, &build, &link, r"^Via(Backwards|Version)-\d+\.\d+\.\d+(-SNAPSHOT)?\.jar$").await;
 
-        // Create a folder in the temp directory named "limonium-via"
-        let created_folder = env::temp_dir().join("limonium-via");
-        if !created_folder.exists() {
-            fs::create_dir(&created_folder).unwrap();
-        }
-
-        // Move the .zip file to the "limonium-via" folder
-        let new_zip_path = created_folder.join("limonium-via.zip");
-        fs::rename(&zip_file_path, &new_zip_path).unwrap();
-
-        // Extract the .zip file to the "limonium-via" folder
-        let output = Command::new("unzip")
-            .arg(&new_zip_path)
-            .current_dir(&created_folder)
-            .output()
-            .expect("Failed to execute command.");
-
-        if !output.status.success() {
-            println!("Extraction failed: {:?}", output);
-            return None;
-        }
-
-        let jar_pattern = Regex::new(r"^Via(Backwards|Version)-\d+\.\d+\.\d+(-SNAPSHOT)?\.jar$").unwrap();
-
-        // Find the .jar files in the libs folder in the "limonium-via" folder
-        let jar_files = fs::read_dir(&created_folder.join("libs"))
-            .unwrap()
-            .filter_map(Result::ok)
-            .filter(|entry| {
-                let path = entry.path();
-                path.is_file() && path.extension().map_or(false, |ext| ext == "jar") && jar_pattern.is_match(path.file_name().unwrap().to_str().unwrap())
-            });
-
-        let mut the_jar_file_path: Option<PathBuf> = None;
-
-        // Find the jar file (should only be one)
-        for jar_file in jar_files {
-            the_jar_file_path = Option::from(jar_file.path());
-        }
-
-        // Don't continue if the jar file was not found
-        if the_jar_file_path.is_none() {
-            println!("{} {}", "Error:".red(), "Failed to find jar file");
-            return None;
-        }
-
-        // Generate a random name for the jar file
-        let random_jar_name = api::random_file_name(&".jar".to_string());
-
-        // Move the jar file to the temp directory
-        let final_jar_path = env::temp_dir().join(&random_jar_name);
-        fs::rename(&the_jar_file_path.unwrap(), &final_jar_path).unwrap();
-
-        // Delete the "limonium-via" folder
-        fs::remove_dir_all(&created_folder).unwrap();
-
-        // Return the name of the jar file
-        let final_jar_file_name = final_jar_path.file_name().unwrap().to_str().unwrap().to_string();
-        Some(final_jar_file_name)
+        return Some(file_name.unwrap());
     }
 }
 
